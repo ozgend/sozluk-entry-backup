1fc255b314b12d9427af94fbb180b5601d212dda
ozgend <revolter00@gmail.com>
2021-01-03 08:30:44 +0300

    added info modal

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,32 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"
     />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css"
+      integrity="sha512-kJ30H6g4NGhWopgdseRb8wTsyllFUYIx3hiUwmGAkgA9B/JbzUBDQVr2VVlWGde6sdBVOG7oU8AL35ORDuMm8g=="
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+      integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+      crossorigin="anonymous"
+    />
+    <InfoBar />
     <EntryBackup />
   </div>
 </template>
 
 <script>
 import EntryBackup from "./components/EntryBackup.vue";
+import InfoBar from "./components/InfoBar.vue";
 
 export default {
   name: "App",
   components: {
     EntryBackup,
+    InfoBar,
   },
 };
 </script>
---
+++ src/components/InfoBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav
+      class="navbar has-background-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand px-3">
+        <a class="button is-primary my-2" @click="isInfoVisible = true">
+          <i class="fa fa-archive" aria-hidden="true"></i>
+        </a>
+      </div>
+    </nav>
+
+    <div
+      class="modal has-text-primary has-background-dark"
+      :class="{ 'is-active': isInfoVisible }"
+    >
+      <div class="modal-background" @click="isInfoVisible = false"></div>
+      <div class="modal-card has-background-dark">
+        <header class="modal-card-head has-text-success has-background-dark">
+          <p class="modal-card-title has-text-primary">nasıl çalışır?</p>
+          <button
+            aria-label="close"
+            class="delete"
+            @click="isInfoVisible = false"
+          ></button>
+        </header>
+        <section class="modal-card-body has-text-primary has-background-dark">
+          <h2></h2>
+          <ol>
+            <li>sözlükteki kullanıcı adınızı girin</li>
+            <li>kaydet'e basın</li>
+            <li>
+              sözlük profil sayfanızda gösterilen entryler html olarak
+              kaydedilecektir.
+            </li>
+          </ol>
+        </section>
+        <footer class="modal-card-foot has-text-primary has-background-dark">
+          <span
+            ><a
+              href="https://github.com/ozgend/sozluk-entry-backup"
+              target="_blank"
+              ><i class="fab fa-github"></i> source</a
+            ></span
+          >
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoBar",
+  data: function () {
+    return {
+      isInfoVisible: false,
+    };
+  },
+};
+</script>
