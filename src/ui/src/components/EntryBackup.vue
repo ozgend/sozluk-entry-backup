bd9eea6c340ae419e1b5fda147d36c601aa5a324
ozgend <revolter00@gmail.com>
2021-01-06 03:56:12 +0300

    added file download options

--- src/ui/src/components/EntryBackup.vue
+++ src/ui/src/components/EntryBackup.vue
@@ -10,7 +10,11 @@
             <div class="field">
               <div class="control">
                 <div class="select is-medium has-backround-dark has-text-dark">
-                  <select v-model="selectedDomainId" class="is-large">
+                  <select
+                    v-model="selectedDomainId"
+                    class="is-large"
+                    :disabled="isBusy"
+                  >
                     <option
                       v-for="domain in domains"
                       v-bind:key="domain.id"
@@ -27,6 +31,7 @@
             <div class="field has-addons">
               <p class="control is-large">
                 <input
+                  :disabled="isBusy"
                   class="input is-large is-primary"
                   type="text"
                   placeholder="kullanıcı adı"
@@ -53,6 +58,13 @@
             </div>
             <h3 class="title is-size-5">{{ profileUrl }}</h3>
           </div>
+
+          <div v-if="isError">
+            <p class="my-4">
+              <span class="has-text-danger px-1">{{ errorMessage }}</span>
+            </p>
+          </div>
+
           <div v-if="isBusy">
             <p class="my-4">
               <span
@@ -70,15 +82,68 @@
             </progress>
           </div>
 
-          <div v-if="isCompleted">
-            <p class="my-2">
-              <i class="fas fa-sticky-note"></i>
-              <span class="px-1"
-                >{{ progress.entryCount }} entry getirildi.</span
+          <div v-if="isPreparingFiles">
+            <p class="my-4">
+              <i class="fas fa-copy mx-1"></i>
+              <span
+                >{{ progress.entryCount }} entry iceren yedekleme dosyaları
+                hazırlanıyor</span
               >
-              <span class="has-text-danger px-1" v-if="isError">{{
-                errorMessage
-              }}</span>
+            </p>
+
+            <progress
+              class="progress is-info"
+              max="100"
+              :attr="false"
+            ></progress>
+          </div>
+
+          <div v-if="isDownloadReady">
+            <p class="my-3">
+              <i class="fas fa-check-circle mx-1"></i>
+              <span class="px-1"
+                ><b>{{ progress.maxPage }}</b> sayfada
+                <b>{{ progress.entryCount }}</b> entry yedeklendi.
+              </span>
+            </p>
+
+            <p class="my-3">
+              <i class="fas fa-file-export mx-1"></i>
+              <span class="px-1"
+                >pdf, html, json ya da hepsini içeren zip dosyasını aşağıdaki
+                bağlantılardan kaydedebilirsiniz.
+              </span>
+            </p>
+
+            <p class="buttons my-4">
+              <a
+                class="button is-info is-normal"
+                :href="`${serviceUrl}/download/${sid}`"
+                target="_blank"
+                :download="`entry-backup_${username}.zip`"
+                >{{ renderResult.zipFilename }}</a
+              >
+              <a
+                class="button is-info is-normal"
+                :href="`${serviceUrl}/download/${sid}/${renderResult.pdfFilename}`"
+                target="_blank"
+                :download="`entry-backup_${username}.pdf`"
+                >{{ renderResult.pdfFilename }}</a
+              >
+              <a
+                class="button is-info is-normal"
+                :href="`${serviceUrl}/download/${sid}/${renderResult.htmlFilename}`"
+                target="_blank"
+                :download="`entry-backup_${username}.html`"
+                >{{ renderResult.htmlFilename }}</a
+              >
+              <a
+                class="button is-info is-normal"
+                :href="`${serviceUrl}/download/${sid}/${renderResult.jsonFilename}`"
+                target="_blank"
+                :download="`entry-backup_${username}.json`"
+                >{{ renderResult.jsonFilename }}</a
+              >
             </p>
           </div>
         </div>
@@ -89,8 +154,6 @@
 
 <script>
 import { downloadUserEntries } from "../parser";
-
-const _sanitizeRegex = new RegExp("\\s", "g");
 
 const _domains = [
   {
@@ -112,30 +175,34 @@ export default {
   name: "EntryBackup",
   data: function () {
     return {
+      sid: null,
       isBusy: false,
       isCompleted: false,
       isCancelled: false,
       isError: false,
+      isPreparingFiles: false,
+      isDownloadReady: false,
       errorMessage: null,
       progress: Object.assign({}, _progress),
       domains: _domains,
       selectedDomainId: "uludag",
       username: null,
-      entries: [],
+      renderResult: null,
     };
   },
   sockets: {
     connect() {
-      console.log("socket connected");
+      console.debug("connected");
     },
-    onSyncSid(id) {
-      console.log(`++ onSyncSid sid=${id}`);
-    },
-    onBuildCompleted() {
-      console.log(`++ onBuildCompleted`);
+    onSyncSid(sid) {
+      console.debug(`++ onSyncSid sid=${sid}`);
+      this.sid = sid;
     },
     onRenderCompleted(result) {
-      console.log(`++ onRenderCompleted ${result}`);
+      console.debug(`++ onRenderCompleted`);
+      this.renderResult = result;
+      this.isPreparingFiles = false;
+      this.isDownloadReady = true;
     },
   },
   computed: {
@@ -154,16 +221,23 @@ export default {
         : "";
     },
     sanitizedUsername() {
-      return this.username.replace(_sanitizeRegex, "-");
+      return this.username.replace(/\s/g, "-");
+    },
+    serviceUrl() {
+      console.log(this.sid);
+      return process.env.VUE_APP_API_HOST || "";
     },
   },
   methods: {
     resetProgress() {
       Object.assign({}, _progress);
-      this.isCompleted = true;
+      this.isCompleted = false;
       this.isBusy = false;
       this.isError = false;
       this.errorMessage = null;
+      this.isPreparingFiles = false;
+      this.isDownloadReady = false;
+      this.renderResult = null;
     },
 
     resetCancellation() {
@@ -207,23 +281,25 @@ export default {
 
       if (this.progress.currentPage === this.progress.maxPage) {
         this.resetProgress();
+        this.isCompleted = true;
         this.beginRender();
         return;
       }
 
       if (this.isCancelled) {
-        this.resetProgress();
+        this.cancelRequest();
         return;
       }
     },
 
     cancelRequest() {
       this.isCancelled = true;
-      // this.$socket.client.emit("onCancelRequest");
-      this.$socket.client.emit("onBeginRender", { username: this.username });
+      this.$socket.client.emit("onCancelRequest");
+      // this.$socket.client.emit("onBeginRender", { username: this.username });
     },
 
     beginRender() {
+      this.isPreparingFiles = true;
       this.$socket.client.emit("onBeginRender", { username: this.username });
     },
   },
